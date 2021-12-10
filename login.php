9c49435d56f985229060e7e367b5628ae1484e33
jmgamez123 <95873640+jmgamez123@users.noreply.github.com>
2021-12-10 15:35:42 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,26 +1,24 @@
 <?php session_start(); 
-include_once('../includes/config.php');
+include_once('includes/config.php');
 // Code for login 
 if(isset($_POST['login']))
 {
-  $adminusername=$_POST['username'];
-  $pass=md5($_POST['password']);
-$ret=mysqli_query($con,"SELECT * FROM admin WHERE username='$adminusername' and password='$pass'");
+$password=$_POST['password'];
+$dec_password=$password;
+$useremail=$_POST['uemail'];
+$ret= mysqli_query($con,"SELECT id,fname FROM users WHERE email='$useremail' and password='$dec_password'");
 $num=mysqli_fetch_array($ret);
 if($num>0)
 {
-$extra="dashboard.php";
-$_SESSION['login']=$_POST['username'];
-$_SESSION['adminid']=$num['id'];
-echo "<script>window.location.href='".$extra."'</script>";
-exit();
+
+$_SESSION['id']=$num['id'];
+$_SESSION['name']=$num['fname'];
+header("location:welcome.php");
+
 }
 else
 {
 echo "<script>alert('Invalid username or password');</script>";
-$extra="index.php";
-echo "<script>window.location.href='".$extra."'</script>";
-exit();
 }
 }
 ?>
@@ -33,11 +31,11 @@ exit();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Admin Login | Registration and Login System</title>
-        <link href="../css/styles.css" rel="stylesheet" />
+        <title>User Login</title>
+        <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-primary">
+    <body class="bg-primary", style="background:url(wp1836700.jpg)">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
@@ -49,14 +47,14 @@ exit();
 <div class="card-header">
 <h2 align="center">Registration and Login System</h2>
 <hr />
-    <h3 class="text-center font-weight-light my-4">Admin Login</h3></div>
+    <h3 class="text-center font-weight-light my-4">User Login</h3></div>
                                     <div class="card-body">
                                         
                                         <form method="post">
                                             
 <div class="form-floating mb-3">
-<input class="form-control" name="username" type="text" placeholder="Username"  required/>
-<label for="inputEmail">Username</label>
+<input class="form-control" name="uemail" type="email" placeholder="name@example.com" required/>
+<label for="inputEmail">Email address</label>
 </div>
                                             
 
@@ -73,7 +71,8 @@ exit();
 </form>
 </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="../index.php">Back to Home Page</a></div>
+                                        <div class="small"><a href="signup.php">Need an account? Sign up!</a></div>
+                                          <div class="small"><a href="index.php">Back to Home</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -83,6 +82,6 @@ exit();
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../js/scripts.js"></script>
+        <script src="js/scripts.js"></script>
     </body>
 </html>
